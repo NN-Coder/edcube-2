2dbf8f9e9ffdfa87d34e0a25a486ed28e8b2114f
Neil <neilnabar7@gmail.com>
2025-04-03 21:44:02 -0700

    Update script.js

--- games/script.js
+++ games/script.js
@@ -1,7 +1,6 @@
     const iframe = document.querySelector('iframe');
     const fullscreenBtn = document.getElementById('fullscreen-button');
 
-    // Function to make the iframe fullscreen
     function goFullscreen() {
         if (iframe.requestFullscreen) {
             iframe.requestFullscreen();
@@ -16,21 +15,17 @@
         }
     }
 
-    // Event listener for button click
     fullscreenBtn.addEventListener('click', function() {
         goFullscreen();
     });
 
-    // Event listener for 'f' key press to trigger fullscreen
     document.addEventListener('keydown', function(event) {
         if (event.key === 'f' || event.key === 'F') {
             goFullscreen();
         }
     });
 document.addEventListener('keydown', function(event) {
-    // Check if the '[' key is pressed 
     if (event.key === '[') {
-        // Redirect to the desired page
         window.location.href = '../index.html'; // Replace with the actual URL
     }
 });
