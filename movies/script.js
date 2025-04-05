a2574b5125cad58274a6b35f1838618dddb4b5a0
Neil Nabar <neilnabar7@gmail.com>
2025-04-05 10:33:23 -0700

    fixed fullscreen for movie pages

---
+++ movies/script.js
@@ -0,0 +1,32 @@
+    const iframe = document.querySelector('iframe');
+    const fullscreenBtn = document.getElementById('fullscreen-button');
+
+    function goFullscreen() {
+        if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+        } else if (iframe.mozRequestFullScreen) { // Firefox
+            iframe.mozRequestFullScreen();
+        } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari, Opera
+            iframe.webkitRequestFullscreen();
+        } else if (iframe.msRequestFullscreen) { // IE/Edge
+            iframe.msRequestFullscreen();
+        } else {
+            alert("Fullscreen is not supported by this browser.");
+        }
+    }
+
+    fullscreenBtn.addEventListener('click', function() {
+        goFullscreen();
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'f' || event.key === 'F') {
+            goFullscreen();
+        }
+    });
+document.addEventListener('keydown', function(event) {
+    if (event.key === '[') {
+        window.location.href = '../index.html'; // Replace with the actual URL
+    }
+});
+
